a2c84ada8b6e800f6f7b0b3cc0d1593129cfdf09
cd01220 <10861479@qq.com>
2016-01-21 10:33:13 +0800

    ...

--- CreateVcProject.py
+++ CreateVcProject.py
@@ -2,6 +2,8 @@ import os
 import shutil
 
 #remove source codes folder, ignor errors.
-shutil.rmtree("ACE_wrappers", True)
+if os.path.exists("ACE_wrappers"):
+    shutil.rmtree("ACE_wrappers")
+
 shutil.unpack_archive("ACE-6.3.0.zip")
 shutil.copy("./config.h", "./ACE_wrappers/ace")
